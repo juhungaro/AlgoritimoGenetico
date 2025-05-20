5eec439d12c16d41b5f50e9a0382afadfef2a5e7
Juliana Hungaro <61634756+juhungaro@users.noreply.github.com>
2025-05-19 22:58:27 -0300

    Update algoritimogenetico.py

--- algoritimogenetico.py
+++ algoritimogenetico.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/13ei1vwISGyRQs9HRQgtlhzPjOKVyyBa2
 """
 
-!pip install streamlit
+
 import streamlit as st
 import random
 import numpy as np
